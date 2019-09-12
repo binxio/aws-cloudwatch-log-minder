33b73e21108a02b546f32c7342b81465accaed09
Mark van Holsteijn <markvanholsteijn@binx.io>
2019-09-12 11:43:11 +0200

    increase # of retries to avoid throttle

--- src/aws_cloudwatch_log_minder/delete_empty_log_streams.py
+++ src/aws_cloudwatch_log_minder/delete_empty_log_streams.py
@@ -1,10 +1,12 @@
-import os
-import boto3
-from botocore.exceptions import ClientError
-from .logging import log
 from datetime import datetime
 
-cw_logs = boto3.client("logs")
+import boto3
+from botocore.config import Config
+from botocore.exceptions import ClientError
+
+from .logging import log
+
+cw_logs = boto3.client("logs", config=Config(retries=dict(max_attempts=10)))
 
 
 def _delete_empty_log_streams(group: dict, oldest_in_ms: int, dry_run: bool):
@@ -13,6 +15,7 @@ def _delete_empty_log_streams(group: dict, oldest_in_ms: int, dry_run: bool):
         "logGroupName": log_group_name,
         "orderBy": "LastEventTime",
         "descending": False,
+        "limit": 50,
     }
     for response in cw_logs.get_paginator("describe_log_streams").paginate(**kwargs):
         for stream in response["logStreams"]:
@@ -55,16 +58,13 @@ def _delete_empty_log_streams(group: dict, oldest_in_ms: int, dry_run: bool):
 def delete_empty_log_streams(dry_run: bool = False):
     log.info("cleaning empty log streams older than the retention period of the group")
     now = (datetime.utcnow() - datetime(1970, 1, 1)).total_seconds()
-    for response in cw_logs.get_paginator("describe_log_groups").paginate():
+    for response in cw_logs.get_paginator("describe_log_groups").paginate(limit=50):
         for group in response["logGroups"]:
             if group.get("retentionInDays"):
                 oldest = now - (group.get("retentionInDays") * 24 * 3600)
                 _delete_empty_log_streams(group, oldest * 1000, dry_run)
             else:
-                log.debug(
-                    "no retention set on log group %s",
-                    log_group_name,
-                )
+                log.debug("no retention set on log group %s", log_group_name)
 
 
 def handle(request: dict = {}, context: dict = {}):
