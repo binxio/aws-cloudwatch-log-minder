8137d8d87d7610815246f095a6de4e394dc73d5d
Mark van Holsteijn <markvanholsteijn@binx.io>
2019-09-12 15:14:24 +0200

    black formatted

--- src/aws_cloudwatch_log_minder/delete_empty_log_streams.py
+++ src/aws_cloudwatch_log_minder/delete_empty_log_streams.py
@@ -10,17 +10,18 @@ from .logger import log
 
 cw_logs = boto3.client("logs", config=Config(retries=dict(max_attempts=10)))
 
+
 def ms_to_datetime(ms: int) -> datetime:
     return datetime(1970, 1, 1) + timedelta(milliseconds=ms)
 
-def _delete_empty_log_streams(group: dict, dry_run:bool = False):
+
+def _delete_empty_log_streams(group: dict, dry_run: bool = False):
     now = datetime.utcnow()
     log_group_name = group["logGroupName"]
     retention_in_days = group["retentionInDays"]
     if not retention_in_days:
         log.info(
-            "skipping log group %s as it has not retention period set",
-            log_group_name,
+            "skipping log group %s as it has not retention period set", log_group_name
         )
         return
 
@@ -34,14 +35,16 @@ def _delete_empty_log_streams(group: dict, dry_run:bool = False):
         "logGroupName": log_group_name,
         "orderBy": "LastEventTime",
         "descending": False,
-        "PaginationConfig": {"MaxItems": 100}
+        "PaginationConfig": {"MaxItems": 100},
     }
 
     for response in cw_logs.get_paginator("describe_log_streams").paginate(**kwargs):
         for stream in response["logStreams"]:
             stored_bytes = stream["storedBytes"]
             log_stream_name = stream["logStreamName"]
-            last_event = ms_to_datetime(stream.get("lastEventTimestamp", stream.get("creationTime")))
+            last_event = ms_to_datetime(
+                stream.get("lastEventTimestamp", stream.get("creationTime"))
+            )
             if last_event > (now - timedelta(days=retention_in_days)):
                 log.info(
                     "there are no log streams from group %s older than the retention period of %s days",
@@ -84,21 +87,25 @@ def _delete_empty_log_streams(group: dict, dry_run:bool = False):
 
 
 def delete_empty_log_streams(dry_run: bool = False, log_group_name_prefix: str = None):
-    kwargs = {"PaginationConfig": {'MaxItems': 100}}
+    kwargs = {"PaginationConfig": {"MaxItems": 100}}
     if log_group_name_prefix:
         kwargs["logGroupNamePrefix"] = log_group_name_prefix
 
     for response in cw_logs.get_paginator("describe_log_groups").paginate(**kwargs):
         for group in response["logGroups"]:
-                _delete_empty_log_streams(group, dry_run)
+            _delete_empty_log_streams(group, dry_run)
+
 
 def get_all_log_group_names() -> List[str]:
     result: List[str] = []
-    for response in cw_logs.get_paginator("describe_log_groups").paginate(PaginationConfig={'MaxItems': 100}):
+    for response in cw_logs.get_paginator("describe_log_groups").paginate(
+        PaginationConfig={"MaxItems": 100}
+    ):
         result.extend(list(map(lambda g: g["logGroupName"], response["logGroups"])))
     return result
 
-def fan_out(function_arn:str, dry_run: bool, log_group_names: List[str]):
+
+def fan_out(function_arn: str, dry_run: bool, log_group_names: List[str]):
     awslambda = boto3.client("lambda")
     log.info(
         "recursively invoking %s to delete from log streams from %d log groups",
@@ -106,12 +113,11 @@ def fan_out(function_arn:str, dry_run: bool, log_group_names: List[str]):
         len(log_group_names),
     )
     for log_group_name in log_group_names:
-        args = json.dumps(
-            {"dry_run": dry_run, "log_group_name_prefix": log_group_name}
-        )
+        args = json.dumps({"dry_run": dry_run, "log_group_name_prefix": log_group_name})
         awslambda.invoke(
             FunctionName=function_arn, InvocationType="Event", Payload=args
         )
+
 
 def handle(request, context):
     dry_run = request.get("dry_run", False)
@@ -123,4 +129,3 @@ def handle(request, context):
         delete_empty_log_streams(dry_run, log_group_name_prefix)
     else:
         fan_out(context.invoked_function_arn, dry_run, get_all_log_group_names())
-
