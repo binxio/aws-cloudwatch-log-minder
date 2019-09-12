aa8f8126558e251969bd446d9853d734e93ffd87
Mark van Holsteijn <markvanholsteijn@binx.io>
2019-09-12 10:08:40 +0200

    bumped to version 0.1.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ dependencies = ['boto3', 'click']
 
 setup(
     name='aws-cloudwatch-log-minder',
-    version="0.1.1",
+    version="0.1.2",
     url='https://github.com/binxio/aws-cloudwatch-log-minder',
     license='BSD',
     author='Mark van Holsteijn',
