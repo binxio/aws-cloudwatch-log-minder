52bafc7b7a302ec57ac946efda055cd20e58e303
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2020-02-07 09:42:06 +0100

    fixed console output

--- src/aws_cloudwatch_log_minder/logger.py
+++ src/aws_cloudwatch_log_minder/logger.py
@@ -1,5 +1,5 @@
 import os
 import logging
 
+logging.basicConfig(format="%(levelname)s: %(message)s", level=os.getenv("LOG_LEVEL", "INFO"))
 log = logging.getLogger()
-log.setLevel(os.getenv("LOG_LEVEL", "INFO"))
