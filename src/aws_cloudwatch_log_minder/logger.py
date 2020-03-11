975e93c896d4023e8ea322035d7ed022ca343dfe
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2020-03-11 07:39:00 +0100

    also work on cmd line

--- src/aws_cloudwatch_log_minder/logger.py
+++ src/aws_cloudwatch_log_minder/logger.py
@@ -1,11 +1,9 @@
 import os
 import logging
 
-<<<<<<< HEAD
-logging.basicConfig(
-    format="%(levelname)s: %(message)s", level=os.getenv("LOG_LEVEL", "INFO")
-)
-=======
->>>>>>> 694e90a02027a76345d6a3e9cdb9712f90a08b29
+if len(logging.getLogger().handlers) == 0:
+    logging.basicConfig(
+        format="%(levelname)s: %(message)s", level=os.getenv("LOG_LEVEL", "INFO")
+    )
 log = logging.getLogger()
 log.setLevel(os.getenv("LOG_LEVEL", "INFO"))
